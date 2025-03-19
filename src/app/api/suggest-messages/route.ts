acbcca824f10f877d4049d5b89d01c86ee34cf88
shiv <sk.sk.shivukumar@gmail.com>
2025-03-19 23:02:38 +0530

    fetch queries added

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,31 +1,66 @@
-import { createGroq } from '@ai-sdk/groq';
-import { generateText } from 'ai';
-
-const groq = createGroq({
-    apiKey : process.env.GROQ_MODEL_API_KEY
-});
-
-import { wrapLanguageModel, extractReasoningMiddleware } from 'ai';
-
-const enhancedModel = wrapLanguageModel({
-  model: groq('llama3-70b-8192'),
-  middleware: extractReasoningMiddleware({ tagName: 'think' }),
-});
-
-const { text } = await generateText({
-    model: enhancedModel,
-    prompt: 'write a essay on a your model',
-  });
+import dbConnect from "@/lib/dbConnect";
+import QueryModel from "@/model/Query";
 
   export async function GET(request:Request) {
-    return new Response(
-        JSON.stringify({
-          success: false,
-          data : text,
-          message: "Not Authenticated",
-        }),
+    dbConnect()
+
+    try{
+       const queries = await QueryModel.aggregate([
         {
-          status: 200,
+          $lookup: {
+            from: "users", // The collection you're joining with (note: collection names are usually pluralized)
+            localField: "owner", // The field in the QueryModel that holds the owner's ID
+            foreignField: "_id", // The field in the User collection that matches the owner's ID
+            as: "owner", // The array to store the matching user documents
+          },
+        },
+        {
+          $unwind: { // Unwind the "owner" array to merge it into a single object
+            path: "$owner",
+            preserveNullAndEmptyArrays: true, // Preserve documents without an owner
+          },
+        },
+        {
+          $set :{
+            owner : "$owner.username"
+          }
         }
-      );
+        ,
+        {
+          $project: {
+            owner: 1, // Include the owner field (now a single object, not an array)
+            query: 1, // Include the query field
+            likes: 1, // Include the likes field
+            createdAt: 1, // Include the createdAt field
+          },
+        },
+        {
+          $sort: { likes: -1  , createdAt :-1}, // Sort by updatedAt field in descending order
+        },
+           
+          ])
+
+          return new Response(
+            JSON.stringify({
+             success: true,
+             data : queries,
+             message: "queries fetched successfully",
+           }),
+           {
+             status: 200,
+           }
+         );
+      
+    }catch(error){
+      return new Response(
+        JSON.stringify({
+         success: true,
+         message: "failed"
+       }),
+       {
+         status: 500,
+       }
+     );
+  
+    }
   }
--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -4,37 +4,68 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
 import { formatTimeAgo } from '@/lib/utils'
+import { querySchema } from '@/schemas/querySchema'
+import axios from 'axios'
 import { Dot, Mail, MessageCircle, Share, Share2 } from 'lucide-react'
-import React, { use } from 'react'
+import { useRouter } from 'next/navigation'
+import React, {useEffect, useState } from 'react'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 const messages = [{
-
+  _id : "123",
   owner : "John Doe",
   query : "Hey, I'm new here",
   createdAt : new Date().toISOString(),
   likes : 5
 },
 {
+  _id : "124",
   owner : "Jane Doe",
   query : "Hey, I'm new here",
   createdAt : Date.now()-1000000000,
   likes : 5
 },
 {
+  _id : "125",
   owner : "John Doe",
   query : "Hey, I'm new here",
   createdAt : new Date().toISOString(),
   likes:8
 }]
 const ExporePage = () => {
+  const [messages , setMessages] = useState<z.infer<typeof querySchema>>()
 
+  const router = useRouter()
+
+  const handleClick = (id : string)=>{
+    router.replace(`/post/${id}`)
+  }
+
+  useEffect(
+    ()=>{
+      const fetchQueries = async()=>{
+        try {
+          const response = await axios.get(`/api/suggest-messages`)
+          console.log(response.data)
+          setMessages(response.data.data)
+        } catch (error) {
+          if(axios.isAxiosError(error)){
+            toast(error.response?.data.message)
+          }
+        }
+      }
+      fetchQueries()
+    }
+    ,[]
+  )
   
   return (
     <div className=' flex-col items-center justify-center'>
     
-        {messages.map((message, index) => (
-            <div className="flex items-center justify-center " key={index}>
-              <Card className='border hover:border-accent-foreground rounded-xl w-full max-w-lg md:max-w-xl'>
+        {messages && messages.map((message : z.infer<typeof querySchema>, index : number) => (
+            <div className="flex items-center justify-center " key={index} >
+              <Card className='border hover:border-accent-foreground rounded-xl w-full max-w-lg md:m ax-w-xl' onClick={()=>handleClick(message._id)}>
                   <CardHeader>
                     <CardTitle className='flex items-center space-x-2'>
                     <Avatar className='h-12 w-12'>
