0a3bc34fad6db0e25b5cf617e280f383433200f4
shiv <sk.sk.shivukumar@gmail.com>
2025-04-07 14:36:42 +0530

    session problem fixed

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,14 +1,17 @@
 'use client'
 
+import { Loader2 } from "lucide-react"
 import {  SessionProvider } from "next-auth/react"
-
+import { Suspense } from "react"
 
 export default function AuthProvider({
   children
 } : { children: React.ReactNode }) {
   return (
     <SessionProvider  >
+      <Suspense fallback={<div className="flex items-center justify-center h-screen"><Loader2 className="animate-spin"></Loader2></div>}>
           {children}
+      </Suspense>
     </SessionProvider>
   )
 }
